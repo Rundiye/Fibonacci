84693ae1e3ec2520199bf33ae01b6aab59296e49
Jundi Ye <rundiye93@gmail.com>
2019-06-24 17:38:09 +0200

    My first script

---
+++ main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var limit = 30;
+var a = 0;
+var b = 1;
+
+/*
+while (a <= limit) {
+    console.log(a);
+    var temp = a + b;
+    a = b;
+    b = temp;
+};*/
+
+for (var a = 0; a <= limit; a--) {
+    console.log(a);
+    var temp = a + b;
+    a = b;
+    b = temp;
+
+    
+    
+}
+
+
+
+
+
+
+
+
